98959ed670a1f0649b0a0406efe602f36b0e4500
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-06-30 14:40:23 +0200

    AddHealthChecks

--- Headers/Program.cs
+++ Headers/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHealthChecks();
 builder.Services.AddAuthentication("certscheme")
     .AddCertificate("certscheme", o =>
     {
